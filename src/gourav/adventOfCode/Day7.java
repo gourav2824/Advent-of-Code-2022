0bc004e978bb12afc22cefc5277b357fed204a3c
Gourav <gourav2824@gmail.com>
2022-12-07 18:05:46 +0530

    Refactor puzzle solutions for day 7

--- src/gourav/adventOfCode/Day7.java
+++ src/gourav/adventOfCode/Day7.java
@@ -9,48 +9,69 @@ import java.util.TreeSet;
 import static gourav.adventOfCode.utils.AOCUtils.getListOfStringsFromInput;
 
 public class Day7 {
-    public static void main(String[] args) {
-        final List<String> input = getListOfStringsFromInput();
-        System.out.println("Total Space = " + puzzle1(input));
-        System.out.println("Puzzle 1 Answer = " + puzzle1Answer);
-        System.out.println("Puzzle 2 Answer = " + puzzle2(root));
-    }
+    private static int puzzle1Answer = 0;
+    private static final TreeSet<Integer> dirSizes = new TreeSet<>();
 
-    private static long puzzle1Answer = 0;
-    private static final Dir root = new Dir();
-    private static final TreeSet<Long> sizes = new TreeSet<>();
-
-    public static class Dir {
+    private static class Dir {
         Map<String, Dir> childDirs;
-        Map<String, Long> fileSizes;
+        Map<String, Integer> fileSizes;
 
-        public Dir() {
+        private Dir() {
             childDirs = new HashMap<>();
             fileSizes = new HashMap<>();
         }
     }
 
-    private static long puzzle1(List<String> input) {
-        final int n = input.size();
+    public static void main(String[] args) {
+        final Dir root = createTreeFromInput(getListOfStringsFromInput());
+        final int totalUsedSpace = calculateDirSize(root);
+        System.out.println("Puzzle 1 Answer = " + puzzle1Answer);
+        System.out.println("Puzzle 2 Answer = " + puzzle2(totalUsedSpace));
+    }
+
+    private static int puzzle2(int totalUsedSpace) {
+        final int requiredSpace = totalUsedSpace - 40000000;
+        final Integer answer = dirSizes.ceiling(requiredSpace);
+        return answer == null ? -1 : answer;
+    }
+
+    private static int calculateDirSize(Dir dir) {
+        int size = 0;
+        for (int val : dir.fileSizes.values()) {
+            size += val;
+        }
+
+        for (Dir childDir : dir.childDirs.values()) {
+            size += calculateDirSize(childDir);
+        }
+
+        if (size <= 100000) {
+            puzzle1Answer += size;
+        }
+
+        dirSizes.add(size);
+        return size;
+    }
+
+    private static Dir createTreeFromInput(List<String> input) {
         final Stack<Dir> st = new Stack<>();
+        final Dir root = new Dir();
+        int i = 0, n = input.size();
         st.push(root);
 
-        for (int i = 0; i < n; i++) {
-            final String command = input.get(i);
+        while (i < n) {
+            final String command = input.get(i++);
             if (command.split(" ")[1].equals("cd")) {
                 final String dirName = command.split(" ")[2];
                 if (dirName.equals("..")) {
                     st.pop();
-                }
-                else {
+                } else {
                     if (!st.peek().childDirs.containsKey(dirName)) {
                         st.peek().childDirs.put(dirName, new Dir());
                     }
                     st.push(st.peek().childDirs.get(dirName));
                 }
-            }
-            else {
-                i++;
+            } else {
                 while (i < n && input.get(i).charAt(0) != '$') {
                     final String content = input.get(i);
                     if (content.split(" ")[0].equals("dir")) {
@@ -58,42 +79,16 @@ public class Day7 {
                         if (!st.peek().childDirs.containsKey(dirName)) {
                             st.peek().childDirs.put(dirName, new Dir());
                         }
-                    }
-                    else {
+                    } else {
                         final String fileName = content.split(" ")[1];
-                        long fileSize = Long.parseLong(content.split(" ")[0]);
+                        int fileSize = Integer.parseInt(content.split(" ")[0]);
                         st.peek().fileSizes.put(fileName, fileSize);
                     }
                     i++;
                 }
-                i--;
             }
         }
 
-        return getDirSize(root);
-    }
-
-    private static long puzzle2(Dir root) {
-        final long usedSpace = getDirSize(root);
-        final long requiredSpace = usedSpace - 40000000;
-        return sizes.ceiling(requiredSpace);
-    }
-
-    private static long getDirSize(Dir dir) {
-        long size = 0;
-        for (long val : dir.fileSizes.values()) {
-            size += val;
-        }
-
-        for (Dir childDir : dir.childDirs.values()) {
-            size += getDirSize(childDir);
-        }
-
-        if (size <= 100000) {
-            puzzle1Answer += size;
-        }
-
-        sizes.add(size);
-        return size;
+        return root;
     }
 }
