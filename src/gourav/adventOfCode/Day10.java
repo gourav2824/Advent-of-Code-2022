29ecd0fce6d70e93b4c8478e1662f5b04c12f48d
Gourav <gourav2824@gmail.com>
2022-12-11 19:28:59 +0530

    Add puzzle 2 solution for day 10

--- src/gourav/adventOfCode/Day10.java
+++ src/gourav/adventOfCode/Day10.java
@@ -9,6 +9,8 @@ public class Day10 {
     public static void main(String[] args) {
         final List<String> commands = getListOfStringsFromInput();
         System.out.println("Puzzle 1 Answer = " + puzzle1(commands));
+        System.out.println("Puzzle 2 Answer:");
+        displayCRT(puzzle2(commands));
     }
 
     private static int puzzle1(List<String> commands) {
@@ -31,5 +33,44 @@ public class Day10 {
         }
 
         return sum;
+    }
+
+    private static char[][] puzzle2(List<String> commands) {
+        final int m = 6, n = 40;
+        final char[][] crt = new char[m][n];
+        int x = 1, cycle = -1;
+
+        for (String command : commands) {
+            cycle++;
+            if (cycle == m * n) break;
+            drawPixel(crt, cycle, x);
+
+            if (!command.equals("noop")) {
+                cycle++;
+                if (cycle == m * n) break;
+                drawPixel(crt, cycle, x);
+                x += Integer.parseInt(command.split(" ")[1]);
+            }
+        }
+
+        return crt;
+    }
+
+    private static void drawPixel(char[][] crt, int cycle, int x) {
+        final int n = 40;
+        if (Math.abs((cycle % n) - x) <= 1) {
+            crt[cycle / n][cycle % n] = '#';
+        } else {
+            crt[cycle / n][cycle % n] = '.';
+        }
+    }
+
+    private static void displayCRT(char[][] crt) {
+        for (char[] row : crt) {
+            for (char pixel : row) {
+                System.out.print(pixel);
+            }
+            System.out.println();
+        }
     }
 }
