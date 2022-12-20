1727ed95fed32cb0767823d9d8c73e58b4ac6bc8
Gourav <gourav2824@gmail.com>
2022-12-20 20:27:31 +0530

    Add puzzle 2 solution for day 20

--- src/gourav/adventOfCode/Day20.java
+++ src/gourav/adventOfCode/Day20.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 
 public class Day20 {
     private static class Num {
-        int val;
-        int pos;
+        long val;
+        final int pos;
 
         public Num(int val, int pos) {
             this.val = val;
@@ -17,41 +17,39 @@ public class Day20 {
 
     public static void main(String[] args) {
         final List<Num> listOfNumbers = getListOfNumbersFromInput();
-        System.out.println("Puzzle 1 Answer = " + puzzle1(listOfNumbers));
+        System.out.println("Puzzle 1 Answer = " + puzzle1(new ArrayList<>(listOfNumbers)));
+        System.out.println("Puzzle 2 Answer = " + puzzle2(new ArrayList<>(listOfNumbers)));
     }
 
-    private static int puzzle1(List<Num> nums) {
+    private static long puzzle1(List<Num> nums) {
         final int n = nums.size();
         mixTheNumbers(nums);
+        return getSum(getIndexOfZero(nums), n, nums);
+    }
 
-        int index = -1;
-        for (int i = 0; i < n; i++) {
-            if (nums.get(i).val == 0) {
-                index = i;
-                break;
-            }
+    private static long puzzle2(List<Num> nums) {
+        final int n = nums.size();
+        final long decryptionKey = 811589153;
+
+        for (Num num : nums) {
+            num.val *= decryptionKey;
         }
 
-        int sum = 0;
-
-        for (int i = 1; i <= 3000; i++) {
-            index = (index + 1) % n;
-            if (i == 1000 || i == 2000 || i == 3000) {
-                sum += nums.get(index).val;
-            }
+        for (int i = 0; i < 10; i++) {
+            mixTheNumbers(nums);
         }
 
-        return sum;
+        return getSum(getIndexOfZero(nums), n, nums);
     }
 
     private static void mixTheNumbers(List<Num> nums) {
         final int n = nums.size();
         for (int pos = 0; pos < n; pos++) {
             int index = getNumIndex(nums, pos);
-            int val = nums.get(index).val;
+            long val = nums.get(index).val;
 
             boolean forward = val >= 0;
-            val = (Math.abs(val) % n) + (Math.abs(val) / n);
+            val = getValue(n, Math.abs(val));
 
             while (val-- > 0) {
                 if (forward) {
@@ -85,6 +83,35 @@ public class Day20 {
                 }
             }
         }
+    }
+
+    private static int getIndexOfZero(List<Num> nums) {
+        int index = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i).val == 0) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    private static long getSum(int index, int n, List<Num> nums) {
+        long sum = 0;
+        for (int i = 1; i <= 3000; i++) {
+            index = (index + 1) % n;
+            if (i == 1000 || i == 2000 || i == 3000) {
+                sum += nums.get(index).val;
+            }
+        }
+        return sum;
+    }
+
+    private static long getValue(int n, long val) {
+        if (val / n > n) {
+            return (val % n) + getValue(n, val / n);
+        }
+        return (val % n) + (val / n);
     }
 
     private static int getNumIndex(List<Num> nums, int pos) {
