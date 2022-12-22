075bc04ef8bd04550d4f141d3553e7f244164b57
Gourav <gourav2824@gmail.com>
2022-12-23 01:47:49 +0530

    Add puzzle 2 solution for day 22

--- src/gourav/adventOfCode/Day22.java
+++ src/gourav/adventOfCode/Day22.java
@@ -11,6 +11,7 @@ public class Day22 {
     private static List<String> inputBoard;
     private static String inputCommandString;
     private static final int[] startPoint = new int[2];
+    private static int n;
 
     public static void main(String[] args) {
         getInputBoardAndCommandStringFromInput();
@@ -18,11 +19,12 @@ public class Day22 {
         final List<String> commands = getListOfCommandsFromCommandString();
 
         System.out.println("Puzzle 1 Answer = " + puzzle1(board, commands));
+        System.out.println("Puzzle 2 Answer = " + puzzle2(board, commands));
     }
 
     private static int puzzle1(Map<List<Integer>, Character> board, List<String> commands) {
         int dir = 0;
-        int[] curr = startPoint;
+        int[] curr = new int[]{startPoint[0], startPoint[1]};
 
         for (String command : commands) {
             if (command.equals("L") || command.equals("R")) {
@@ -30,7 +32,7 @@ public class Day22 {
             } else {
                 int val = Integer.parseInt(command);
                 while (val-- > 0) {
-                    if (!move(board, dir, curr)) {
+                    if (!moveAroundTheBoard(board, dir, curr)) {
                         break;
                     }
                 }
@@ -40,7 +42,7 @@ public class Day22 {
         return 1000 * curr[0] + 4 * curr[1] + dir;
     }
 
-    private static boolean move(Map<List<Integer>, Character> board, int dir, int[] curr) {
+    private static boolean moveAroundTheBoard(Map<List<Integer>, Character> board, int dir, int[] curr) {
         int r = curr[0], c = curr[1];
         switch (dir) {
             case 0:
@@ -86,6 +88,213 @@ public class Day22 {
             curr[1] = c;
             return true;
         }
+    }
+
+    // The puzzle 2 solution will only work for a specific cube having the following shape:
+    //          . . 1 .
+    //          2 3 4 .
+    //          . . 5 6
+    // The above shown cube faces are handled separately in the solution.
+
+    private static int puzzle2(Map<List<Integer>, Character> board, List<String> commands) {
+        int[] curr = new int[]{startPoint[0], startPoint[1], 0};
+
+        for (String command : commands) {
+            if (command.equals("L") || command.equals("R")) {
+                curr[2] = changeDirection(curr[2], command);
+            } else {
+                int val = Integer.parseInt(command);
+                while (val-- > 0) {
+                    if (!moveAroundTheCube(board, curr)) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return 1000 * curr[0] + 4 * curr[1] + curr[2];
+    }
+
+    // N -> Normal Cases, S -> Special Cases (Around the Cube)
+    // Conditions for all 6 cube faces are shown using comments.
+    private static boolean moveAroundTheCube(Map<List<Integer>, Character> board, int[] curr) {
+        int r = curr[0], c = curr[1], dir = curr[2];
+
+        if (r <= n) {
+            // Face 1
+            if (dir == 0) {
+                if (c % n == 0) {
+                    // S
+                    return move(board, curr, (n * 3) - (r - 1), n * 4, 2);
+                } else {
+                    // N
+                    return move(board, curr, r, c + 1, dir);
+                }
+            } else if (dir == 1) {
+                // N
+                return move(board, curr, r + 1, c, dir);
+            } else if (dir == 2) {
+                if (c % n == 1) {
+                    // S
+                    return move(board, curr, n + 1, n + r, 1);
+                } else {
+                    // N
+                    return move(board, curr, r, c - 1, dir);
+                }
+            } else {
+                if (r % n == 1) {
+                    // S
+                    return move(board, curr, n + 1, (c % n == 0) ? 1 : n - ((c % n) - 1), 1);
+                } else {
+                    // N
+                    return move(board, curr, r - 1, c, dir);
+                }
+            }
+        } else if (r <= n * 2) {
+            if (c <= n) {
+                // Face 2
+                if (dir == 0) {
+                    // N
+                    return move(board, curr, r, c + 1, dir);
+                } else if (dir == 1) {
+                    if (r % n == 0) {
+                        // S
+                        return move(board, curr, n * 3, (n * 3) - (c - 1), 3);
+                    } else {
+                        // N
+                        return move(board, curr, r + 1, c, dir);
+                    }
+                } else if (dir == 2) {
+                    if (c == 1) {
+                        // S
+                        return move(board, curr, n * 3, (r % n == 0) ? ((n * 3) + 1) : (n * 4) - (r % n - 1), 3);
+                    } else {
+                        // N
+                        return move(board, curr, r, c - 1, dir);
+                    }
+                } else {
+                    if (r % n == 1) {
+                        // S
+                        return move(board, curr, 1, (n * 3) - (c - 1), 1);
+                    } else {
+                        // N
+                        return move(board, curr, r - 1, c, dir);
+                    }
+                }
+            } else if (c <= n * 2) {
+                // Face 3
+                if (dir == 0) {
+                    // N
+                    return move(board, curr, r, c + 1, dir);
+                } else if (dir == 1) {
+                    if (r % n == 0) {
+                        // S
+                        return move(board, curr, (c % n == 0) ? ((n * 2) + 1) : (n * 3) - ((c % n) - 1), (n * 2) + 1, 0);
+                    } else {
+                        // N
+                        return move(board, curr, r + 1, c, dir);
+                    }
+                } else if (dir == 2) {
+                    // N
+                    return move(board, curr, r, c - 1, dir);
+                } else {
+                    if (r % n == 1) {
+                        // S
+                        return move(board, curr, (c % n == 0) ? n : (c % n), (n * 2) + 1, 0);
+                    } else {
+                        // N
+                        return move(board, curr, r - 1, c, dir);
+                    }
+                }
+            } else {
+                // Face 4
+                if (dir == 0) {
+                    if (c % n == 0) {
+                        // S
+                        return move(board, curr, (n * 2) + 1, (r % n == 0) ? ((n * 3) + 1) : (n * 4) - ((r % n) - 1), 1);
+                    } else {
+                        // N
+                        return move(board, curr, r, c + 1, dir);
+                    }
+                } else if (dir == 1) {
+                    // N
+                    return move(board, curr, r + 1, c, dir);
+                } else if (dir == 2) {
+                    // N
+                    return move(board, curr, r, c - 1, dir);
+                } else {
+                    // N
+                    return move(board, curr, r - 1, c, dir);
+                }
+            }
+        } else {
+            if (c <= n * 3) {
+                // Face 5
+                if (dir == 0) {
+                    // N
+                    return move(board, curr, r, c + 1, dir);
+                } else if (dir == 1) {
+                    if (r % n == 0) {
+                        // S
+                        return move(board, curr, n * 2, (c % n == 0) ? 1 : n - (c % n) + 1, 3);
+                    } else {
+                        // N
+                        return move(board, curr, r + 1, c, dir);
+                    }
+                } else if (dir == 2) {
+                    if (c % n == 1) {
+                        // S
+                        return move(board, curr, n * 2, (r % n == 0) ? (n + 1) : (n * 2) - ((r % n) - 1), 3);
+                    } else {
+                        // N
+                        return move(board, curr, r, c - 1, dir);
+                    }
+                } else {
+                    // N
+                    return move(board, curr, r - 1, c, dir);
+                }
+            } else {
+                // Face 6
+                if (dir == 0) {
+                    if (c % n == 0) {
+                        // S
+                        return move(board, curr, (r % n == 0) ? 1 : n - ((r % n) - 1), n * 3, 2);
+                    } else {
+                        // N
+                        return move(board, curr, r, c + 1, dir);
+                    }
+                } else if (dir == 1) {
+                    if (r % n == 0) {
+                        // S
+                        return move(board, curr, (c % n == 0) ? (n + 1) : (n * 2) - ((c % n) - 1), 1, 0);
+                    } else {
+                        // N
+                        return move(board, curr, r + 1, c, dir);
+                    }
+                } else if (dir == 2) {
+                    // N
+                    return move(board, curr, r, c - 1, dir);
+                } else {
+                    if (r % n == 1) {
+                        // S
+                        return move(board, curr, (c % n == 0) ? (n + 1) : (n * 2) - ((c % n) - 1), (n * 3), 2);
+                    } else {
+                        // N
+                        return move(board, curr, r - 1, c, dir);
+                    }
+                }
+            }
+        }
+    }
+
+    private static boolean move(Map<List<Integer>, Character> board, int[] curr, int r, int c, int dir) {
+        if (board.get(Arrays.asList(r, c)) == '#') {
+            return false;
+        }
+        curr[0] = r;
+        curr[1] = c;
+        curr[2] = dir;
+        return true;
     }
 
     private static int changeDirection(int dir, String command) {
@@ -149,6 +358,7 @@ public class Day22 {
             input = sc.nextLine();
         }
 
+        n = inputBoard.size() / 3;
         inputCommandString = sc.nextLine();
         sc.close();
     }
